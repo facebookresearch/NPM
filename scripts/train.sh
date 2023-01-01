527c02cb8ef482cb0e6cf6e5a79a20b4b87dfd66
Sewon Min <sewonmin@fb.com>
2023-01-01 09:47:13 +0900

    add copyright headers

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+# Copyright (c) Facebook, Inc. and its affiliates.
+# All rights reserved.
+# 
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
 
 SAVE_DIR=$1
 DO_PHRASE=$2
